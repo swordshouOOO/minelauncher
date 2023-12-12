f5859d0f469ef45f94a30895ea9090538109e88f
swordshouOOO <122036764+swordshouOOO@users.noreply.github.com>
2023-12-12 09:40:22 +0300

    config.ts

--- config.ts
+++ config.ts
@@ -7,7 +7,7 @@ export const window = {
     resizable: false,
     maximizable: false,
     fullscreenable: false,
-    title: 'Aurora Launcher',
+    title: 'MineGashLauncher',
 };
 
 export const api = {
